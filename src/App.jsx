edb308f246c7519d97504a53ce923c59d1f7a543
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-10-15 20:27:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" w-20 h-20 flex justify-center items-center text-red-500 bg-slate-200">
+        Ecom
+      </div>
+    </>
+  );
+}
+
+export default App;
